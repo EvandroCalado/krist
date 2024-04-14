06762d1d7f0948bec67d09ad470190308f655886
Evandro Calado <evandrocalado07@gmail.com>
2024-04-13 23:13:43 -0300

    🐳 chore: group headings

--- src/components/Heading/Heading.stories.tsx
+++ src/components/Heading/Heading.stories.tsx
@@ -5,9 +5,67 @@ import { Heading, HeadingProps } from './Heading';
 export default {
   title: 'Components/Heading',
   component: Heading,
-  args: {
-    children: 'Heading',
-  },
 } as Meta;
 
-export const Default: StoryFn<HeadingProps> = (args) => <Heading {...args} />;
+export const Headings: StoryFn<HeadingProps> = () => (
+  <div style={{ display: 'flex', gap: '2rem', flexWrap: 'wrap' }}>
+    <div>
+      <Heading size="6xl" transform="capitalize">
+        Heading
+      </Heading>
+      <Heading as="h2" size="5xl" transform="capitalize">
+        Heading
+      </Heading>
+      <Heading as="h3" size="4xl" transform="capitalize">
+        Heading
+      </Heading>
+      <Heading as="h4" size="3xl" transform="capitalize">
+        Heading
+      </Heading>
+      <Heading as="h5" size="2xl" transform="capitalize">
+        Heading
+      </Heading>
+      <Heading as="h6" size="xl" transform="capitalize">
+        Heading
+      </Heading>
+    </div>
+    <div>
+      <Heading size="6xl">Heading</Heading>
+      <Heading as="h2" size="5xl">
+        Heading
+      </Heading>
+      <Heading as="h3" size="4xl">
+        Heading
+      </Heading>
+      <Heading as="h4" size="3xl">
+        Heading
+      </Heading>
+      <Heading as="h5" size="2xl">
+        Heading
+      </Heading>
+      <Heading as="h6" size="xl">
+        Heading
+      </Heading>
+    </div>
+    <div>
+      <Heading size="6xl" transform="uppercase">
+        Heading
+      </Heading>
+      <Heading as="h2" size="5xl" transform="uppercase">
+        Heading
+      </Heading>
+      <Heading as="h3" size="4xl" transform="uppercase">
+        Heading
+      </Heading>
+      <Heading as="h4" size="3xl" transform="uppercase">
+        Heading
+      </Heading>
+      <Heading as="h5" size="2xl" transform="uppercase">
+        Heading
+      </Heading>
+      <Heading as="h6" size="xl" transform="uppercase">
+        Heading
+      </Heading>
+    </div>
+  </div>
+);
