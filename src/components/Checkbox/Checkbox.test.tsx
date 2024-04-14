e22103e65cc84424489b448305cac30b4fffa132
Evandro Calado <evandrocalado07@gmail.com>
2024-04-14 17:59:22 -0300

    🧪 test: add test

---
+++ src/components/Checkbox/Checkbox.test.tsx
@@ -0,0 +1,39 @@
+import { fireEvent, screen } from '@testing-library/react';
+
+import { customRender } from 'utils/test-utils';
+
+import { Checkbox } from './Checkbox';
+
+describe('Checkbox', () => {
+  it('should render default', () => {
+    customRender(<Checkbox label="checkbox" name="checkbox" />);
+
+    const label = screen.getByLabelText('checkbox');
+    const input = screen.getByRole('checkbox');
+
+    expect(label).toBeInTheDocument();
+    expect(input).toHaveAttribute('name', 'checkbox');
+    expect(input).toHaveAttribute('type', 'checkbox');
+  });
+
+  it('should render on click', () => {
+    customRender(<Checkbox label="checkbox" name="checkbox" />);
+
+    const label = screen.getByLabelText('checkbox');
+    const input = screen.getByRole('checkbox');
+
+    expect(input).not.toBeChecked();
+
+    fireEvent.click(label);
+
+    expect(input).toBeChecked();
+  });
+
+  it('should render a snapshot', () => {
+    const { container } = customRender(
+      <Checkbox label="checkbox" name="checkbox" />,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+});
