0fa0ae8ae3541bdecbe88a67877a7ae3c9234b48
Evandro Calado <evandrocalado07@gmail.com>
2024-04-14 17:56:46 -0300

    🐳 chore: add buttons

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -11,17 +11,23 @@ export default {
 export const Buttons: StoryFn<ButtonProps> = () => (
   <div style={{ display: 'flex', gap: '1rem', flexWrap: 'wrap' }}>
     <Button>primary</Button>
+
     <Button variant="secondary">secondary</Button>
+
     <Button variant="secondary">
       <MoveRight size={16} />
     </Button>
+
     <Button>
       <MoveRight size={16} />
     </Button>
+
     <Button variant="secondary">
       <Heart size={16} />
     </Button>
+
     <Button disabled>disabled</Button>
+
     <Button width="100%">full width</Button>
   </div>
 );
