881e1a47138c6988517601e67b67f25b20e07518
Evandro Calado <evandrocalado07@gmail.com>
2024-04-14 23:17:00 -0300

    ✨ feat: add react router dom
    create home, login, register and 404 routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import { Counter } from 'components';
+import { Login } from 'pages';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello world!</div>,
+    errorElement: <div>Error</div>,
+    children: [],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+    errorElement: <div>Login</div>,
+  },
+  {
+    path: '/register',
+    element: <div>register</div>,
+    errorElement: <div>Register</div>,
+  },
+  {
+    path: '*',
+    element: <div>Not found</div>,
+    errorElement: <div>Not found</div>,
+  },
+]);
 
 const App = () => {
-  return (
-    <main>
-      <Counter />
-    </main>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
