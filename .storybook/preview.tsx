0daa0c6c4f6b89ed72f63a0f0e9c4a8a3da51298
Evandro Calado <evandrocalado07@gmail.com>
2024-04-15 20:35:44 -0300

    🐳 chore: configs to react router dom

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { withRouter } from 'storybook-addon-remix-react-router';
 import { ThemeProvider } from 'styled-components';
 
 import { GlobalStyles } from '../src/styles/globals';
 import theme from '../src/styles/theme';
 
 export const decorators = [
+  withRouter,
   (Story) => (
     <ThemeProvider theme={theme}>
       <Story />
--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -1,8 +1,13 @@
 import { RenderResult, render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
 import theme from 'styles/theme';
 
 export const customRender = (children: React.ReactNode): RenderResult => {
-  return render(<ThemeProvider theme={theme}>{children}</ThemeProvider>);
+  return render(
+    <ThemeProvider theme={theme}>
+      <MemoryRouter>{children}</MemoryRouter>
+    </ThemeProvider>,
+  );
 };
