c9dd18a517ea5bc81a77d51937f0d1a330014114
Evandro Calado <evandrocalado07@gmail.com>
2024-04-15 20:36:23 -0300

    🐳 chore: add stories

---
+++ src/components/MenuNav/MenuNav.stories.tsx
@@ -0,0 +1,10 @@
+import { Meta, StoryFn } from '@storybook/react';
+
+import { MenuNav } from '.';
+
+export default {
+  title: 'Components/MenuNav',
+  component: MenuNav,
+} as Meta;
+
+export const Default: StoryFn = () => <MenuNav />;
