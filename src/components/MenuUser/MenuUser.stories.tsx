e2a43d3dcdc74762bf1f131953d40cdf8f3c5e07
Evandro Calado <evandrocalado07@gmail.com>
2024-04-15 23:11:03 -0300

    🐳 chore: add stories

---
+++ src/components/MenuUser/MenuUser.stories.tsx
@@ -0,0 +1,10 @@
+import { Meta, StoryFn } from '@storybook/react';
+
+import { MenuUser } from '.';
+
+export default {
+  title: 'Components/ MenuUser',
+  component: MenuUser,
+} as Meta;
+
+export const Default: StoryFn = () => <MenuUser />;
