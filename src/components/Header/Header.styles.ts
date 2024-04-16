e3b37c68fe52cc465ec7db3d45ca1e9e0601f25b
Evandro Calado <evandrocalado07@gmail.com>
2024-04-16 08:13:41 -0300

    🐳 chore: hidden menus

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -6,5 +6,15 @@ export const Header = styled.header`
     align-items: center;
     justify-content: space-between;
     padding: ${theme.spacings.md} ${theme.spacings['5xl']};
+
+    @media ${theme.media.lteOrEqMedium} {
+      .menu-nav {
+        display: none;
+      }
+
+      .menu-user {
+        display: none;
+      }
+    }
   `}
 `;
