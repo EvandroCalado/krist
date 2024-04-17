e3ce5254820e9805ccdef8123803f76654cd4bf6
Evandro Calado <evandrocalado07@gmail.com>
2024-04-17 09:13:43 -0300

    🐳 chore: add navigate

--- src/components/MenuUser/MenuUser.tsx
+++ src/components/MenuUser/MenuUser.tsx
@@ -1,16 +1,19 @@
 import { Heart, ShoppingBag } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
 
 import { Button } from 'components';
 
 import * as S from './MenuUser.styles';
 
 export const MenuUser = () => {
+  const navigate = useNavigate();
+
   return (
     <S.Container className="menu-user">
       <span>Search</span>
       <Heart />
       <ShoppingBag />
-      <Button>Entrar</Button>
+      <Button onClick={() => navigate('/login')}>Entrar</Button>
     </S.Container>
   );
 };
