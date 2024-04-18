b4e84bf6261ceb7c717817de04c62aa95b2763e5
Evandro Calado <evandrocalado07@gmail.com>
2024-04-17 23:14:53 -0300

    🐳 chore: add stories

---
+++ src/components/CategoryCard/CategoryCard.stories.tsx
@@ -0,0 +1,17 @@
+import { Meta, StoryFn } from '@storybook/react';
+
+import { CategoryCard, CategoryCardProps } from '.';
+
+export default {
+  title: 'Components/CategoryCard',
+  args: {
+    title: 'masculino',
+    image: '/products/camiseta/1.webp',
+    buttonLabel: 'masculino',
+  },
+  component: CategoryCard,
+} as Meta;
+
+export const Default: StoryFn<CategoryCardProps> = (args) => (
+  <CategoryCard {...args} />
+);
