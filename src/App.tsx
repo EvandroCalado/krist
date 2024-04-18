e8127fc494626237c85179e71e47256c4c77fbd6
Evandro Calado <evandrocalado07@gmail.com>
2024-04-17 23:16:26 -0300

    🐳 chore: create route index for home

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,66 @@
 import { Home, Login, Register } from 'pages';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-export const router = createBrowserRouter([
+import { CategoriesCarousel, Hero } from 'components';
+
+const categories = [
+  {
+    title: 'masculino',
+    image: '/products/camiseta/1.webp',
+    buttonLabel: 'masculino',
+  },
+  {
+    title: 'femenino',
+    image: '/products/camiseta/1.webp',
+    buttonLabel: 'femenino',
+  },
+  {
+    title: 'unisex',
+    image: '/products/camiseta/1.webp',
+    buttonLabel: 'unisex',
+  },
+  {
+    title: 'kids',
+    image: '/products/camiseta/1.webp',
+    buttonLabel: 'kids',
+  },
+  {
+    title: 'esportivo',
+    image: '/products/camiseta/1.webp',
+    buttonLabel: 'esportivo',
+  },
+];
+
+const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
-    errorElement: <div>Error</div>,
-    children: [],
+    errorElement: <div>Home error</div>,
+    children: [
+      {
+        index: true,
+        element: [
+          <Hero key={1} />,
+          <CategoriesCarousel categories={categories} key={2} />,
+        ],
+        errorElement: <div>Home error</div>,
+      },
+    ],
   },
   {
     path: '/login',
     element: <Login />,
-    errorElement: <div>Login</div>,
+    errorElement: <div>Login error</div>,
   },
   {
     path: '/register',
     element: <Register />,
-    errorElement: <div>Register</div>,
+    errorElement: <div>Register error</div>,
   },
   {
     path: '*',
     element: <div>Not found</div>,
-    errorElement: <div>Not found</div>,
+    errorElement: <div>Not found error</div>,
   },
 ]);
 
