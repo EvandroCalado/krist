d7d97f8828d0f69525d43abc73f7751a570e6c46
Evandro Calado <evandrocalado07@gmail.com>
2024-04-19 10:35:02 -0300

    🐳 chore: update stories

--- src/components/RatingsCarousel/RatingsCarousel.stories.tsx
+++ src/components/RatingsCarousel/RatingsCarousel.stories.tsx
@@ -15,7 +15,7 @@ export const ratings = [
   },
   {
     id: 2,
-    rating: 3,
+    rating: 1,
     ratingDescription:
       'Esta camisa é de alta qualidade, com um tecido suave e durável. Seu design simples, mas elegante, a torna versátil para diversas ocasiões. O ajuste é confortável, permitindo liberdade de movimento, e a camisa mantém sua forma mesmo após várias lavagens. O preço é justo considerando sua qualidade e durabilidade. Em geral, é uma excelente escolha.',
     avatar:
@@ -35,7 +35,17 @@ export const ratings = [
   },
   {
     id: 4,
-    rating: 3,
+    rating: 2,
+    ratingDescription:
+      'Esta camisa é de alta qualidade, com um tecido suave e durável. Seu design simples, mas elegante, a torna versátil para diversas ocasiões. O ajuste é confortável, permitindo liberdade de movimento, e a camisa mantém sua forma mesmo após várias lavagens. O preço é justo considerando sua qualidade e durabilidade. Em geral, é uma excelente escolha.',
+    avatar:
+      'https://img.freepik.com/free-psd/3d-render-avatar-character_23-2150611762.jpg?w=826&t=st=1713491585~exp=1713492185~hmac=08ba90f79cafc9a795e191d5f3833a9f1f73184a0d936d5a6e0187a4c100d8fe',
+    name: 'joão',
+    profession: 'programador',
+  },
+  {
+    id: 5,
+    rating: 4,
     ratingDescription:
       'Esta camisa é de alta qualidade, com um tecido suave e durável. Seu design simples, mas elegante, a torna versátil para diversas ocasiões. O ajuste é confortável, permitindo liberdade de movimento, e a camisa mantém sua forma mesmo após várias lavagens. O preço é justo considerando sua qualidade e durabilidade. Em geral, é uma excelente escolha.',
     avatar:
