a1ad304e0bddbf53367470a4381eba9ea740e8f6
Evandro Calado <evandrocalado07@gmail.com>
2024-04-20 22:39:09 -0300

    🐳 chore: add footer

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom';
 
-import { Header } from 'components';
+import { Footer, Header } from 'components';
 
 export const Home = () => {
   return (
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   );
 };
