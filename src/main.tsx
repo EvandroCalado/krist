0f980fe544ab8aa26c41f364fbb03426202534ba
Evandro Calado <evandrocalado07@gmail.com>
2024-04-22 09:34:04 -0300

    🐞 fix: import store

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import { Toaster } from 'react-hot-toast';
 import { Provider } from 'react-redux';
-import { store } from 'store.ts';
 import { ThemeProvider } from 'styled-components';
 
 import { GlobalStyles } from 'styles/globals.ts';
 import theme from 'styles/theme.ts';
 
 import App from './App.tsx';
+import { store } from './store.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
