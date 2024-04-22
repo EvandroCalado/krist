f7b9fde6a46d9cd1dd31ae0160badb04c7882a8b
Evandro Calado <evandrocalado07@gmail.com>
2024-04-21 22:24:02 -0300

    🌈 style: add media queries

--- src/components/RatingsCarousel/RatingsCarousel.styles.ts
+++ src/components/RatingsCarousel/RatingsCarousel.styles.ts
@@ -14,6 +14,10 @@ export const Carousel = styled(Splide)`
   align-items: center;
   justify-content: center;
   padding: 0 ${({ theme }) => theme.spacings['10xl']};
+
+  @media ${({ theme }) => theme.media.lteOrEqMedium} {
+    padding: 0 ${({ theme }) => theme.spacings.xl};
+  }
 `;
 
 export const CarouselSlide = styled(SplideSlide)`
