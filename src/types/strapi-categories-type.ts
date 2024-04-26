a512201edd8c1f281f68bbe14ce88fb68858e7fb
Evandro Calado <evandrocalado07@gmail.com>
2024-04-26 09:48:02 -0300

    🐳 chore: create categories type

---
+++ src/types/strapi-categories-type.ts
@@ -0,0 +1,82 @@
+export interface StrapiCategoriesType {
+  data: Datum[];
+  meta: Meta;
+}
+
+interface Meta {
+  pagination: Pagination;
+}
+
+interface Pagination {
+  page: number;
+  pageSize: number;
+  pageCount: number;
+  total: number;
+}
+
+interface Datum {
+  id: number;
+  attributes: Attributes2;
+}
+
+interface Attributes2 {
+  name: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  cover: Cover;
+}
+
+interface Cover {
+  data: Data;
+}
+
+interface Data {
+  id: number;
+  attributes: Attributes;
+}
+
+interface Attributes {
+  name: string;
+  alternativeText: null;
+  caption: null;
+  width: number;
+  height: number;
+  formats: Formats;
+  hash: string;
+  ext: string;
+  mime: string;
+  size: number;
+  url: string;
+  previewUrl: null;
+  provider: string;
+  provider_metadata: Providermetadata;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface Formats {
+  large: Large;
+  small: Large;
+  medium: Large;
+  thumbnail: Large;
+}
+
+interface Large {
+  ext: string;
+  url: string;
+  hash: string;
+  mime: string;
+  name: string;
+  path: null;
+  size: number;
+  width: number;
+  height: number;
+  sizeInBytes: number;
+  provider_metadata: Providermetadata;
+}
+
+interface Providermetadata {
+  public_id: string;
+  resource_type: string;
+}
