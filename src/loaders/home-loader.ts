e6b584278d76b97b8d387f2c7a0fe0726916c8e9
Evandro Calado <evandrocalado07@gmail.com>
2024-04-26 19:59:20 -0300

    🐳 chore: create get all featured products

--- src/loaders/home-loader.ts
+++ src/loaders/home-loader.ts
@@ -2,6 +2,7 @@ import { AxiosError } from 'axios';
 
 import { StrapiCategoriesType } from 'types/strapi-categories-type';
 import { StrapiConfigType } from 'types/strapi-config-type';
+import { StrapiFeaturedType } from 'types/strapi-featured-type';
 import { customFetch } from 'utils/custom-fetch';
 
 export const homeLoader = async () => {
@@ -12,14 +13,22 @@ export const homeLoader = async () => {
     const categories = await customFetch.get<StrapiCategoriesType>(
       'categories?populate[0]=cover',
     );
+    const featured = await customFetch.get<StrapiFeaturedType>(
+      '/products?populate=deep,3&filters[isFeatured][$eq]=true',
+    );
 
-    if (config.status !== 200 || categories.status !== 200) {
+    if (
+      config.status !== 200 ||
+      categories.status !== 200 ||
+      featured.status !== 200
+    ) {
       throw new Error('not found');
     }
 
     return {
       config: config.data,
       categories: categories.data,
+      products: featured.data,
     };
   } catch (error) {
     if (error instanceof AxiosError) {
