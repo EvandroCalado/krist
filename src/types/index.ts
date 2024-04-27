3a17b733c846775782092606c2b8d75b4677853f
Evandro Calado <evandrocalado07@gmail.com>
2024-04-26 23:09:25 -0300

    🐳 chore: add index for types

---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+export * from './strapi-categories-type';
+export * from './strapi-config-type';
+export * from './strapi-featured-type';
+export * from './strapi-flashsale-type';
+export * from './styled-components';
