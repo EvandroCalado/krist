7b843fc5ab7b44bc2170e2f49513a7e7a4098830
Evandro Calado <evandrocalado07@gmail.com>
2024-04-28 16:44:11 -0300

    🐳 chore: add redux provider

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { withRouter } from 'storybook-addon-remix-react-router';
 import { ThemeProvider } from 'styled-components';
 
 import { GlobalStyles } from '../src/styles/globals';
 import theme from '../src/styles/theme';
 
+import { store } from '../src/store';
+
 export const decorators = [
   withRouter,
   (Story) => (
-    <ThemeProvider theme={theme}>
-      <Story />
-      <GlobalStyles />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Story />
+        <GlobalStyles />
+      </ThemeProvider>
+    </Provider>
   ),
 ];
