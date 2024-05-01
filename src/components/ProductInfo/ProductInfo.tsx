a3365408164b78e621079b5139463d813e80137d
Evandro Calado <evandrocalado07@gmail.com>
2024-05-01 20:52:58 -0300

    🐞 fix: remove sizes

--- src/components/ProductInfo/ProductInfo.tsx
+++ src/components/ProductInfo/ProductInfo.tsx
@@ -1,28 +1,20 @@
 import { FC } from 'react';
 
-import { StrapiProductColorsType, StrapiProductSizesType } from 'types';
+import { StrapiProductVariantType } from 'types';
 
 import * as S from './ProductInfo.styles';
 
 export interface ProductInfoProps {
-  colors: StrapiProductColorsType;
-  sizes: StrapiProductSizesType;
+  variants: StrapiProductVariantType[];
 }
 
-export const ProductInfo: FC<ProductInfoProps> = ({ colors, sizes }) => {
+export const ProductInfo: FC<ProductInfoProps> = ({ variants }) => {
   return (
     <S.Container>
       <S.ItemContainer>
         <S.ItemTitle>cores</S.ItemTitle>
-        {colors.data.map((color) => (
-          <S.Item key={color.id}>{color.attributes.name}</S.Item>
-        ))}
-      </S.ItemContainer>
-
-      <S.ItemContainer>
-        <S.ItemTitle>tamanhos</S.ItemTitle>
-        {sizes.data.map((size) => (
-          <S.Item key={size.id}>{size.attributes.name}</S.Item>
+        {variants.map((variant) => (
+          <S.Item key={variant.id}>{variant.name}</S.Item>
         ))}
       </S.ItemContainer>
     </S.Container>
