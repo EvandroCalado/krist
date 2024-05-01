c87a78bfe98bc74c85a1e25bc732969e18db8089
Evandro Calado <evandrocalado07@gmail.com>
2024-05-01 20:32:13 -0300

    🐞 fix: id not discount

--- src/utils/calcDiscount.ts
+++ src/utils/calcDiscount.ts
@@ -1,5 +1,7 @@
 export const calcDiscount = (value: number, discount: number) => {
   const valueWithDiscount = (value * (100 - discount)) / 100;
 
+  if (discount === 0) return value;
+
   return valueWithDiscount.toFixed(2);
 };
