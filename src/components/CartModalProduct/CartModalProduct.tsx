c68b6839e348385a23c08c210f805485e2eb3c18
Evandro Calado <evandrocalado07@gmail.com>
2024-05-04 22:56:43 -0300

    🐞 fix: add table and tbody

--- src/components/CartModalProduct/CartModalProduct.tsx
+++ src/components/CartModalProduct/CartModalProduct.tsx
@@ -9,21 +9,25 @@ export interface CartModalProductProps {}
 
 export const CartModalProduct: FC<CartModalProductProps> = () => {
   return (
-    <S.Tr>
-      <S.ImageTh>
-        <img src="./products/camiseta/1.webp" alt="produto" />
-        <S.TitleContainer>
-          <Heading as="h4" transform="capitalize" fontWeight="600">
-            camiseta masculina manga curta
-          </Heading>
-          <span>1 x R$80.00</span>
-          <span>size: m</span>
-        </S.TitleContainer>
-      </S.ImageTh>
+    <table>
+      <tbody>
+        <S.Tr>
+          <S.ImageTh>
+            <img src="./products/camiseta/1.webp" alt="produto" />
+            <S.TitleContainer>
+              <Heading as="h4" transform="capitalize" fontWeight="600">
+                camiseta masculina manga curta
+              </Heading>
+              <span>1 x R$80.00</span>
+              <span>size: m</span>
+            </S.TitleContainer>
+          </S.ImageTh>
 
-      <S.TrashTd>
-        <Trash2 size={18} color="red" />
-      </S.TrashTd>
-    </S.Tr>
+          <S.TrashTd>
+            <Trash2 size={18} color="red" />
+          </S.TrashTd>
+        </S.Tr>
+      </tbody>
+    </table>
   );
 };
