d1bb7c7ab7f35fb1d0daa8a8ab417693e1983f14
Evandro Calado <evandrocalado07@gmail.com>
2024-05-06 20:18:31 -0300

    🐳 chore: add type

--- src/hooks/redux-hook.ts
+++ src/hooks/redux-hook.ts
@@ -1,5 +1,9 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from 'store';
+import {
+  useDispatch,
+  useSelector,
+  type TypedUseSelectorHook,
+} from 'react-redux';
+import type { AppDispatch, RootState } from 'store';
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
