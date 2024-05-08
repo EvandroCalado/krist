04e63f19d5a38d0b7ee2607cdddfa23ab54451ac
Evandro Calado <evandrocalado07@gmail.com>
2024-05-07 21:05:58 -0300

    🐞 fix: remove is black boolean

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -7,14 +7,11 @@ import * as S from './Logo.styles';
 export const Logo = () => {
   const { config } = useLoaderData() as { config: StrapiConfigType };
 
-  const { black, white, isBlack } = config.data.attributes.logo;
+  const { black } = config.data.attributes.logo;
 
   return (
     <Link to="/">
-      <S.Logo
-        src={isBlack ? black.data.attributes.url : white.data.attributes.url}
-        alt="logo"
-      />
+      <S.Logo src={black.data.attributes.url} alt="logo" />
     </Link>
   );
 };
