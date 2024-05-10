4695a9423336a6cb3ca2e98e935bac877ecd8643
Evandro Calado <evandrocalado07@gmail.com>
2024-05-10 10:22:23 -0300

    🌈 style: fix padding

--- src/components/ProductFilter/ProductFilter.styles.ts
+++ src/components/ProductFilter/ProductFilter.styles.ts
@@ -4,7 +4,7 @@ export const Container = styled.div`
   ${({ theme }) => css`
     display: flex;
     flex-direction: column;
-    gap: ${theme.spacings.sm};
+    gap: 0.5rem;
 
     & > h3 {
       margin-bottom: ${theme.spacings.sm};
