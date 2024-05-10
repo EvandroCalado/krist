e0140fb8cb02a9423d19afa54b3adeb40317cc4d
Evandro Calado <evandrocalado07@gmail.com>
2024-05-10 09:14:12 -0300

    🐳 chore: add categories filter

---
+++ src/pages/shop/shop.styles.ts
@@ -0,0 +1,27 @@
+import styled, { css } from 'styled-components';
+
+export const Container = styled.div`
+  ${({ theme }) => css`
+    padding: ${theme.spacings.md} ${theme.spacings['10xl']};
+
+    @media ${theme.media.lteOrEqMedium} {
+      padding: ${theme.spacings.md} ${theme.spacings.lg};
+    }
+  `}
+`;
+
+export const FiltersContainer = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+  `}
+`;
+
+export const Filters = styled.div`
+  ${({ theme }) => css`
+    width: 100%;
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    gap: ${theme.spacings.xl};
+  `}
+`;
--- src/pages/shop/shop.tsx
+++ src/pages/shop/shop.tsx
@@ -1,9 +1,61 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+import { BreadCrumb, Checkbox, ProductFilter } from 'components';
+
+import * as S from './shop.styles';
 
 export const Shop = () => {
+  const [category, setCategory] = useState('');
+
   return (
-    <div style={{ height: '50.5vh', textAlign: 'center' }}>
-      <Link to="/login?transfer=/shop">login</Link>
-    </div>
+    <S.Container>
+      <BreadCrumb />
+
+      <S.FiltersContainer>
+        <S.Filters>
+          <ProductFilter name="categorias">
+            <Checkbox
+              label="homem"
+              name="homem"
+              checked={category === 'homem'}
+              value="homem"
+              onChange={(e) => setCategory(e.target.value)}
+            />
+            <Checkbox
+              label="mulher"
+              name="mulher"
+              checked={category === 'mulher'}
+              value="mulher"
+              onChange={(e) => setCategory(e.target.value)}
+            />
+            <Checkbox
+              label="kids"
+              name="kids"
+              checked={category === 'kids'}
+              value="kids"
+              onChange={(e) => setCategory(e.target.value)}
+            />
+            <Checkbox
+              label="esportivos"
+              name="esportivos"
+              checked={category === 'esportivos'}
+              value="esportivos"
+              onChange={(e) => setCategory(e.target.value)}
+            />
+            <Checkbox
+              label="jaquetas"
+              name="jaquetas"
+              checked={category === 'jaquetas'}
+              value="jaquetas"
+              onChange={(e) => setCategory(e.target.value)}
+            />
+          </ProductFilter>
+          <ProductFilter name="preços">price</ProductFilter>
+          <ProductFilter name="cores">color</ProductFilter>
+          <ProductFilter name="tamanhos">size</ProductFilter>
+        </S.Filters>
+        <div style={{ flex: 5 }}>products</div>
+      </S.FiltersContainer>
+    </S.Container>
   );
 };
