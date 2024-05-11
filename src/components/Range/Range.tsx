9c0bd3abeb250f1cd6c9525507cd470bc977fc1c
Evandro Calado <evandrocalado07@gmail.com>
2024-05-10 21:35:36 -0300

    🐳 chore: add type

--- src/components/Range/Range.tsx
+++ src/components/Range/Range.tsx
@@ -2,17 +2,19 @@ import { FC } from 'react';
 
 import * as S from './Range.styles';
 
-export interface RangeProps {
+export interface RangeProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
   price: number;
   setPrice: (price: number) => void;
 }
 
-export const Range: FC<RangeProps> = ({ price, setPrice }) => {
+export const Range: FC<RangeProps> = ({ price, setPrice, ...rest }) => {
   const maxPrice = 1000;
 
   return (
     <S.Container>
       <S.Input
+        {...rest}
         type="range"
         min="{0}"
         max={maxPrice}
