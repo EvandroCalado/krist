15ba1320b19f5658b0eedd769ad3a8ef634a5c2b
Evandro Calado <evandrocalado07@gmail.com>
2024-05-11 23:54:14 -0300

    🐞 fix: remove title

--- src/components/ProductsList/ProductsList.styles.ts
+++ src/components/ProductsList/ProductsList.styles.ts
@@ -1,19 +1,9 @@
 import styled, { css } from 'styled-components';
 
-export const Container = styled.section`
-  display: flex;
-  flex-direction: column;
-
-  & > h2 {
-    margin: 0 auto;
-  }
-`;
-
 export const GridContainer = styled.div`
   ${({ theme }) => css`
-    padding: ${theme.spacings['10xl']};
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
     gap: 6rem;
 
     @media ${theme.media.lteOrEqMedium} {
--- src/components/ProductsList/ProductsList.tsx
+++ src/components/ProductsList/ProductsList.tsx
@@ -1,24 +1,18 @@
 import { useLoaderData } from 'react-router-dom';
 
-import { Heading } from 'components';
 import { ProductCard } from 'components/ProductCard';
-import { StrapiFeaturedType } from 'types';
+import { StrapiProductsType } from 'types';
 
 import * as S from './ProductsList.styles';
 
 export const ProductsList = () => {
-  const { products } = useLoaderData() as { products: StrapiFeaturedType };
+  const { products } = useLoaderData() as { products: StrapiProductsType };
 
   return (
-    <S.Container>
-      <Heading as="h2" transform="capitalize" size="2xl" fontWeight="600">
-        em destaque
-      </Heading>
-      <S.GridContainer>
-        {products.data.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </S.GridContainer>
-    </S.Container>
+    <S.GridContainer>
+      {products.data.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </S.GridContainer>
   );
 };
