fa844a97378dbf751b8731f0cd4e30b8de11abeb
Evandro Calado <evandrocalado07@gmail.com>
2024-05-13 10:28:37 -0300

    ✨ feat: create checkout page

---
+++ src/pages/Checkout/Checkout.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+export interface CheckoutProps {}
+
+export const Checkout: FC<CheckoutProps> = () => {
+  return (
+    <>
+      <h1>Checkout</h1>
+    </>
+  );
+};
