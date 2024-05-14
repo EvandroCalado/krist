d6d74e6a77d5f709282ee2903d6fe545a69f09c2
Evandro Calado <evandrocalado07@gmail.com>
2024-05-14 20:27:50 -0300

    🐳 chore: add edit and delete address

--- src/components/AddressCard/AddressCard.tsx
+++ src/components/AddressCard/AddressCard.tsx
@@ -1,7 +1,12 @@
+import { AxiosError } from 'axios';
 import { FilePenLine, Trash2 } from 'lucide-react';
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import toast from 'react-hot-toast';
+import { Form } from 'react-router-dom';
 
+import { AddressModal, Button, Input } from 'components';
 import { StrapiAddressType } from 'types';
+import { customFetch } from 'utils';
 
 import * as S from './AddressCard.styles';
 
@@ -10,6 +15,45 @@ export interface AddressCardProps {
 }
 
 export const AddressCard: FC<AddressCardProps> = ({ address }) => {
+  const [openModal, setOpenModal] = useState(false);
+  const [titleInput, setTitleInput] = useState(address.attributes.title);
+  const [addressInput, setAddressInput] = useState(address.attributes.address);
+  const [zipCodeInput, setZipCodeInput] = useState(address.attributes.zipCode);
+
+  const handleDelete = async (id: number) => {
+    try {
+      const addressesResponse = await customFetch.delete(`/addresses/${id}`);
+
+      if (addressesResponse.status !== 200) {
+        toast.error('Erro ao deletar endereço');
+      }
+    } catch (error) {
+      if (error instanceof AxiosError && error.response?.data?.error) {
+        throw new Error(error.response.data.error.message);
+      }
+    }
+  };
+
+  const handleUpdate = async (id: number) => {
+    try {
+      const addressesResponse = await customFetch.put(`/addresses/${id}`, {
+        data: {
+          title: titleInput,
+          address: addressInput,
+          zipCode: zipCodeInput,
+        },
+      });
+
+      if (addressesResponse.status !== 200) {
+        toast.error('Erro ao editar endereço');
+      }
+    } catch (error) {
+      if (error instanceof AxiosError && error.response?.data?.error) {
+        throw new Error(error.response.data.error.message);
+      }
+    }
+  };
+
   return (
     <S.AddressCardContainer>
       <S.AddressCardTitle>
@@ -29,15 +73,48 @@ export const AddressCard: FC<AddressCardProps> = ({ address }) => {
         CEP: {address.attributes.zipCode}
       </S.AddressCardZipcode>
 
-      <S.AddressCardButtons>
-        <button className="edit">
-          <FilePenLine /> editar
-        </button>
+      <Form method="get">
+        <S.AddressCardButtons>
+          <button
+            type="button"
+            onClick={() => setOpenModal(true)}
+            className="edit"
+          >
+            <FilePenLine /> editar
+          </button>
 
-        <button className="delete">
-          <Trash2 /> apagar
-        </button>
-      </S.AddressCardButtons>
+          <AddressModal openModal={openModal} setOpenModal={setOpenModal}>
+            <Input
+              label="nome"
+              type="text"
+              name="title"
+              value={titleInput}
+              onChange={(e) => setTitleInput(e.target.value)}
+            />
+            <Input
+              label="endereço"
+              type="text"
+              name="address"
+              value={addressInput}
+              onChange={(e) => setAddressInput(e.target.value)}
+            />
+            <Input
+              label="CEP"
+              type="number"
+              name="zipCode"
+              value={zipCodeInput}
+              onChange={(e) => setZipCodeInput(e.target.value)}
+            />
+            <Button onClick={() => handleUpdate(address.id)} type="submit">
+              salvar
+            </Button>
+          </AddressModal>
+
+          <button onClick={() => handleDelete(address.id)} className="delete">
+            <Trash2 /> apagar
+          </button>
+        </S.AddressCardButtons>
+      </Form>
     </S.AddressCardContainer>
   );
 };
