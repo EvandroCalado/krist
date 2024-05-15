a0dc2b185ace9aa2845ae3ff5e03946485113912
Evandro Calado <evandrocalado07@gmail.com>
2024-05-14 23:48:17 -0300

    🌈 style: add media queries

--- src/components/Payment/Payment.styles.ts
+++ src/components/Payment/Payment.styles.ts
@@ -2,10 +2,18 @@ import styled, { css } from 'styled-components';
 
 export const Container = styled.div`
   ${({ theme }) => css`
-    width: 50rem;
+    width: 50%;
     display: flex;
     flex-direction: column;
     gap: ${theme.spacings.md};
     margin: ${theme.spacings.xl} 0;
+
+    @media ${theme.media.lteOrEqXLarge} {
+      width: 70%;
+    }
+
+    @media ${theme.media.lteOrEqLarge} {
+      width: 100%;
+    }
   `}
 `;
