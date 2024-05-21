c6951bea95386819a45414bd36e39b99fe9653c7
Evandro Calado <evandrocalado07@gmail.com>
2024-05-21 10:01:39 -0300

    🐳 chore: add stripe payment

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
 import isPropValid from '@emotion/is-prop-valid';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
@@ -12,18 +14,22 @@ import theme from 'styles/theme.ts';
 import App from './App.tsx';
 import { store } from './store.ts';
 
+const stripePromise = loadStripe(import.meta.env.VITE_APP_STRIPE_KEY);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <HelmetProvider>
-        <StyleSheetManager shouldForwardProp={(prop) => isPropValid(prop)}>
-          <ThemeProvider theme={theme}>
-            <Toaster />
-            <App />
-            <GlobalStyles />
-          </ThemeProvider>
-        </StyleSheetManager>
-      </HelmetProvider>
-    </Provider>
+    <Elements stripe={stripePromise}>
+      <Provider store={store}>
+        <HelmetProvider>
+          <StyleSheetManager shouldForwardProp={(prop) => isPropValid(prop)}>
+            <ThemeProvider theme={theme}>
+              <Toaster />
+              <App />
+              <GlobalStyles />
+            </ThemeProvider>
+          </StyleSheetManager>
+        </HelmetProvider>
+      </Provider>
+    </Elements>
   </React.StrictMode>,
 );
