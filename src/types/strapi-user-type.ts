5dd92a31c0e984c4f52894bc8d645efddfe96a8c
Evandro Calado <evandrocalado07@gmail.com>
2024-05-25 11:56:55 -0300

    🐞 fix: add ratings and orders type

--- src/types/strapi-user-type.ts
+++ src/types/strapi-user-type.ts
@@ -12,6 +12,214 @@ export interface StrapiUserType {
   addresses: Address[];
   orders: StrapiOrderType[];
   avatar: Avatar;
+  ratings: Rating2[];
+  wishlists: Wishlist2[];
+}
+
+interface Wishlist2 {
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  user: User4;
+  product: Product3;
+}
+
+interface Product3 {
+  id: number;
+  title: string;
+  subTitle: string;
+  description: string;
+  discountPercentage: null | number;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  isFeatured: boolean;
+  cover: Cover;
+  brand: Brand;
+  categories: Brand[];
+  variants: Variant2[];
+}
+
+interface Variant2 {
+  id: number;
+  name: string;
+  price: number;
+  discountPercentage: number;
+  color: string;
+}
+
+interface User4 {
+  id: number;
+  username: string;
+  email: string;
+  provider: string;
+  confirmed: boolean;
+  blocked: boolean;
+  createdAt: string;
+  updatedAt: string;
+  phone: string;
+  role: Role;
+  avatar: Avatar;
+}
+
+interface Rating2 {
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  rating: string;
+  description: string;
+  user: User3;
+  product: Product2;
+}
+
+interface Product2 {
+  id: number;
+  title: string;
+  subTitle: string;
+  description: string;
+  discountPercentage: number;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  isFeatured: boolean;
+  cover: Cover;
+  brand: Brand;
+  categories: Brand[];
+  variants: Variant[];
+  wishlists: Wishlist2[];
+}
+
+interface Variant {
+  id: number;
+  name: string;
+  price: number;
+  discountPercentage: null | number;
+  color: string;
+}
+
+interface Brand {
+  id: number;
+  name: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+}
+
+interface Cover {
+  id: number;
+  name: string;
+  alternativeText: null;
+  caption: null;
+  width: number;
+  height: number;
+  formats: Formats2;
+  hash: string;
+  ext: string;
+  mime: string;
+  size: number;
+  url: string;
+  previewUrl: null;
+  provider: string;
+  provider_metadata: Providermetadata;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface Formats2 {
+  large: Thumbnail;
+  small: Thumbnail;
+  medium: Thumbnail;
+  thumbnail: Thumbnail;
+}
+
+interface User3 {
+  id: number;
+  username: string;
+  email: string;
+  provider: string;
+  confirmed: boolean;
+  blocked: boolean;
+  createdAt: string;
+  updatedAt: string;
+  phone: string;
+  role: Role;
+  avatar: Avatar;
+  wishlists: Wishlist[];
+}
+
+export interface StrapiOrderType {
+  id: number;
+  products: Product[];
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  amount: string;
+  user: User2;
+}
+
+interface User2 {
+  id: number;
+  username: string;
+  email: string;
+  provider: string;
+  confirmed: boolean;
+  blocked: boolean;
+  createdAt: string;
+  updatedAt: string;
+  phone: string;
+  role: Role;
+  avatar: Avatar;
+  ratings: Rating[];
+  wishlists: Wishlist[];
+}
+
+interface Address {
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  title: string;
+  address: string;
+  zipCode: string;
+  city: string;
+  state: string;
+  country: string;
+  user: User;
+}
+
+interface User {
+  id: number;
+  username: string;
+  email: string;
+  provider: string;
+  confirmed: boolean;
+  blocked: boolean;
+  createdAt: string;
+  updatedAt: string;
+  phone: string;
+  role: Role;
+  orders: Order[];
+  avatar: Avatar;
+  ratings: Rating[];
+  wishlists: Wishlist[];
+}
+
+interface Wishlist {
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+}
+
+interface Rating {
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  rating: string;
+  description: string;
 }
 
 interface Avatar {
@@ -57,7 +265,7 @@ interface Providermetadata {
   resource_type: string;
 }
 
-export interface StrapiOrderType {
+interface Order {
   id: number;
   products: Product[];
   createdAt: string;
@@ -76,19 +284,6 @@ interface Product {
   cartId: string;
   quantity: number;
   productId: number;
-}
-
-interface Address {
-  id: number;
-  createdAt: string;
-  updatedAt: string;
-  publishedAt: string;
-  title: string;
-  address: string;
-  zipCode: string;
-  city: string;
-  state: string;
-  country: string;
 }
 
 interface Role {
