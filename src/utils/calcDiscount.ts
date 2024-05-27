a99e180c4d9bd4052e14e5a9f7d1440623154a71
Evandro Calado <evandrocalado07@gmail.com>
2024-05-26 23:35:23 -0300

    🐳 chore: return price if no have discount

--- src/utils/calcDiscount.ts
+++ src/utils/calcDiscount.ts
@@ -1,7 +1,7 @@
-export const calcDiscount = (value: number, discount: number) => {
-  const valueWithDiscount = (value * (100 - discount)) / 100;
+export const calcDiscount = (value: number, discount: number | null) => {
+  if (!discount) return value;
 
-  if (discount === 0) return value;
+  const valueWithDiscount = (value * (100 - discount)) / 100;
 
   return valueWithDiscount;
 };
