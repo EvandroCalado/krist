40a8e4f36430896adf8c4a10874d356911f93972
Evandro Calado <evandrocalado07@gmail.com>
2024-05-27 10:43:14 -0300

    🐳 chore: create get ratings

--- src/loaders/home-loader.ts
+++ src/loaders/home-loader.ts
@@ -11,22 +11,25 @@ import { customFetch } from 'utils/custom-fetch';
 
 export const homeLoader: LoaderFunction = async () => {
   try {
-    const [config, categories, featured, flashSale] = await Promise.all([
-      customFetch.get<StrapiConfigType>('/config?populate=deep,3'),
-      customFetch.get<StrapiCategoriesType>('categories?populate[0]=cover'),
-      customFetch.get<StrapiFeaturedType>(
-        '/products?populate=deep,3&filters[isFeatured][$eq]=true',
-      ),
-      customFetch.get<StrapiFlashSaleType>('/flash-sales?populate=deep,3'),
-    ]);
+    const [config, categories, featured, ratings, flashSale] =
+      await Promise.all([
+        customFetch.get<StrapiConfigType>('/config?populate=deep,3'),
+        customFetch.get<StrapiCategoriesType>('categories?populate[0]=cover'),
+        customFetch.get<StrapiFeaturedType>(
+          '/products?populate=deep,3&filters[isFeatured][$eq]=true',
+        ),
+        customFetch.get('ratings?populate=deep,3'),
+        customFetch.get<StrapiFlashSaleType>('/flash-sales?populate=deep,3'),
+      ]);
 
-    const [configRes, categoriesRes, featuredRes, flashSaleRes] =
-      await Promise.all([config, categories, featured, flashSale]);
+    const [configRes, categoriesRes, featuredRes, ratingsRes, flashSaleRes] =
+      await Promise.all([config, categories, featured, ratings, flashSale]);
 
     if (
       !configRes?.data ||
       !categoriesRes?.data ||
       !featuredRes?.data ||
+      !ratingsRes?.data ||
       !flashSaleRes?.data
     ) {
       throw new Error('not found');
@@ -36,6 +39,7 @@ export const homeLoader: LoaderFunction = async () => {
       configRes.status !== 200 ||
       categoriesRes.status !== 200 ||
       featuredRes.status !== 200 ||
+      ratingsRes.status !== 200 ||
       flashSaleRes.status !== 200
     ) {
       throw new Error('not found');
@@ -45,6 +49,7 @@ export const homeLoader: LoaderFunction = async () => {
       config: configRes.data,
       categories: categoriesRes.data,
       products: featuredRes.data,
+      ratings: ratingsRes.data,
       flashSale: flashSaleRes.data,
     };
   } catch (error) {
