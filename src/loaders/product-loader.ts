d34cfe9dbfacff9f893d92c0a42c5bf0080a0b42
Evandro Calado <evandrocalado07@gmail.com>
2024-06-04 09:55:31 -0300

    🐳 chore: create get by categories

--- src/loaders/product-loader.ts
+++ src/loaders/product-loader.ts
@@ -1,27 +1,40 @@
 import { AxiosError } from 'axios';
 import { LoaderFunction } from 'react-router-dom';
 
-import { StrapiProductType } from 'types';
+import {
+  StrapiProductDetailsType,
+  StrapiProductsByCategoriesType,
+  StrapiRatingsType,
+} from 'types';
 import { customFetch } from 'utils';
 
 export const productLoader: LoaderFunction = async ({ params }) => {
   const id = params.id;
 
   try {
-    const productsResponse = await customFetch.get<StrapiProductType>(
+    const productsResponse = await customFetch.get<StrapiProductDetailsType>(
       `/products/${id}?populate=deep,3`,
     );
-    const ratingsResponse = await customFetch.get(
+    const ratingsResponse = await customFetch.get<StrapiRatingsType>(
       `/ratings?populate=deep,3&filters[product][$eq]=${id}`,
     );
+    const byCategoriesResponse =
+      await customFetch.get<StrapiProductsByCategoriesType>(
+        `/products?populate=deep,2&filters[categories][name][$eq]=${productsResponse.data.data.attributes.categories.data[0].attributes.name}`,
+      );
 
-    if (productsResponse.status !== 200 || ratingsResponse.status !== 200) {
+    if (
+      productsResponse.status !== 200 ||
+      ratingsResponse.status !== 200 ||
+      byCategoriesResponse.status !== 200
+    ) {
       throw new Error('not found');
     }
 
     return {
       product: productsResponse.data,
       ratings: ratingsResponse.data,
+      byCategories: byCategoriesResponse.data,
     };
   } catch (error) {
     if (error instanceof AxiosError && error.response?.data?.error) {
